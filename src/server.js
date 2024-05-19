40fd3c854b6cb0802cc7be6054a894ee8f65c74d
Arthur <atwiiister@gmail.com>
2024-05-19 11:59:24 -0300

    adição de mais configs

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
+import cors from "cors";
+import express from "express";
+
 const express = require('express')
 const app = express();
 
+
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 3001;
 
@@ -9,6 +14,11 @@ app.get('/', (req, res) =>
 {
     res.send('Teste liga')
 });
+
+app.get('/liga', (req, res) => 
+    {
+        res.send('pegando os dados da liga')
+    });
 
 app.listen(3000, () => {
     console.log('Rodando na porta 3000')
