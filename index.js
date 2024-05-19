1044761124064926281e39af6211e5758b8a748f
Arthur <atwiiister@gmail.com>
2024-05-19 12:22:31 -0300

    dados mocados iniciais

--- index.js
+++ index.js
@@ -13,10 +13,31 @@ app.get('/', (req, res) =>
     res.send('Teste liga')
 });
 
-app.get('/liga', (req, res) => 
-    {
-        res.send('pegando os dados da liga')
-    });
+function createData(posicao, nome, pontuacao) {
+    return {
+      posicao,
+      nome,
+      pontuacao,
+      history: [
+        { data: '2020-01-05', pontuacao: '12' },
+        { data: '2020-01-05', pontuacao: '16' },
+        { data: '2020-01-05', pontuacao: '17' },
+        { data: '2020-01-05', pontuacao: '18' },
+        { data: '2020-01-05', pontuacao: '16' },
+      ],
+    };
+  };
+
+app.get('/liga', (req, res) => {
+const rows = [
+        createData(1, 'Arthur', 60),
+        createData(2, 'Dylan', 59),
+        createData(3, 'Gabriel', 55),
+        createData(4, 'Rica', 40),
+        createData(5, 'RenêRoots', 3),
+      ];
+
+res.send(rows)});
 
 app.listen(port, () => {
     console.log('Rodando na porta 3000')
