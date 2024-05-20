8474a9214a1d02e218217824a4e4beaa7ea2b9a0
Arthur <atwiiister@gmail.com>
2024-05-20 18:38:41 -0300

    troca de banco de dados e criação do novo endpoint para ligas

--- db.js
+++ db.js
@@ -1,28 +1,40 @@
-import mysql from "mysql2";
+import pkg from 'pg';
+const { Client } = pkg;
 
-const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,      
-    database: process.env.DATABASE, 
-    port: 16168,
-    connectionLimit: 10 
-});
+// Database connection configuration
+const dbConfig = {
+    user: "postgres",
+    host: "reverently-exultant-dunlin.data-1.use1.tembo.io",
+    database: "pokeliga",
+    password: "ttnK072q3d6gYMk0",
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+};
 
-// Agora você pode executar consultas usando o pool
-export const query = (sql, values) => {
-  return new Promise((resolve, reject) => {
-    pool.getConnection((err, connection) => {
-      if (err) {
-        return reject(err);
-      }
-      connection.query(sql, values, (err, results) => {
-        connection.release(); // Libera a conexão de volta para o pool
-        if (err) {
-          return reject(err);
-        }
-        resolve(results);
-      });
-    });
-  });
+// user: process.env.USER,
+// host: process.env.HOST,
+// database: process.env.DATABASE,
+// password: process.env.PASSWORD,
+// Create a new PostgreSQL client
+const client = new Client(dbConfig);
+
+// Connect to the database
+export const query = async (query) => {
+  let result;
+    try {
+        await client.connect();
+        console.log('Connected to PostgreSQL database');
+
+        // Execute SQL queries here
+        result = await client.query(query);
+
+        // Close the connection when done
+        await client.end();
+        console.log('Connection to PostgreSQL closed');
+        return result.rows;
+    } catch (err) {
+        console.error('Error connecting to PostgreSQL database', err);
+    }
 };
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import cors from "cors";
 import express from "express";
 import {query} from './db.js';
 import aplicarRegra from './regras.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
@@ -16,6 +18,10 @@ app.get('/', (req, res) => {
   
 app.get('/liga', async (req, res) => {
     res.json(aplicarRegra(await query('SELECT * FROM Resultados'))); 
+});
+
+app.get('/ligas', async (req, res) => {
+    res.json(await query('SELECT * FROM ligas order by DataFim desc')); 
 });
 
 app.listen(port, () => {
